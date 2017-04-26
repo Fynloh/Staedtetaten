57a343561841e2ebeb5ed07a7576a402d12c3f37
admin <admin@PC>
2017-04-26 17:53:35 +0200

    Homework done +add 10 new cities

--- src/SolveTree.java
+++ src/SolveTree.java
@@ -8,19 +8,41 @@ class SolveTree
   {         //knoten
         BinaryTree<String> rechts = new BinaryTree<String>("Am Oberrhein?");
         BinaryTree<String> links  = new BinaryTree<String>("Linksrheinisch?");
-        BinaryTree<String> linksRe  = new BinaryTree<String>("Aachen");
+        BinaryTree<String> linksRe  = new BinaryTree<String>("Gibt es dort eine Rennstrecke?");
+        BinaryTree<String> linksReLi  = new BinaryTree<String>("Aachen");
+        BinaryTree<String> linksReRe  = new BinaryTree<String>("Spa");
         BinaryTree<String> linksLi  = new BinaryTree<String>("An der Weser?");
-        BinaryTree<String> linksLiLi = new BinaryTree<String> ("Hannover");
+        BinaryTree<String> linksLiLi = new BinaryTree<String> ("Aktuelle Hauptstadt von Deutschland?");
+        BinaryTree<String> linksLiLiLi = new BinaryTree<String> ("Im Ruhrgebiet?");
+        BinaryTree<String> linksLiLiRe = new BinaryTree<String> ("Berlin");
+        BinaryTree<String> linksLiLiLiRe = new BinaryTree<String> ("Dortmund");
+        BinaryTree<String> linksLiLiLiLi = new BinaryTree<String> ("Landeshauptstadt von Niedersachsen?");
+        BinaryTree<String> linksLiLiLiLiRe = new BinaryTree<String> ("Hannover");
+        BinaryTree<String> linksLiLiLiLiLi = new BinaryTree<String> ("Minden");
         BinaryTree<String> linksLiRe = new BinaryTree<String> ("Minden");
-        BinaryTree<String> rechtsLi = new BinaryTree<String> ("Koeln");
+        BinaryTree<String> rechtsLi = new BinaryTree<String> ("Ehemalige Hauptstadt Deutschlands?");
+        BinaryTree<String> rechtsLiLi = new BinaryTree<String> ("Koeln");
+        BinaryTree<String> rechtsLiRe = new BinaryTree<String> ("Bonn");
         BinaryTree<String> rechtsRe = new BinaryTree<String> ("Basel");
         
+        
+       
+        linksLiLiLiLi.setLeftTree(linksLiLiLiLiLi);
+        linksLiLiLiLi.setRightTree(linksLiLiLiLiRe);
+        linksLiLiLi.setLeftTree(linksLiLiLiLi);
+        linksLiLiLi.setRightTree(linksLiLiLiRe);
+        linksLiLi.setRightTree(linksLiLiRe);
+        linksLiLi.setLeftTree(linksLiLiLi);
+        linksRe.setRightTree(linksReRe);
+        linksRe.setLeftTree(linksReLi);
         linksLi.setLeftTree (linksLiLi);
         linksLi.setRightTree (linksLiRe);
         links.setLeftTree (linksLi);
         links.setRightTree(linksRe);
         rechts.setLeftTree (rechtsLi);
         rechts.setRightTree(rechtsRe);
+        rechtsLi.setRightTree(rechtsLiRe);
+        rechtsLi.setLeftTree(rechtsLiLi);
         meinBaum.setRightTree (rechts);
         meinBaum.setLeftTree (links);
         start = meinBaum;
